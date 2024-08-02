ba07596350a6390ffd574feaaf9f70951a9bc571
Khairil rud <khairilrudiwibowo@gmail.com>
2024-08-02 22:54:18 +0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,21 @@
+import { Client, GatewayIntentBits, Intents, CommandInteraction } from 'discord.js';
+import { config } from 'dotenv';
+
+config();
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on('messageCreate', async message => {
+    if (message.content === '!ping') {
+        const sent = await message.channel.send('Pinging...');
+        sent.edit(`Pong! Latency is ${sent.createdTimestamp - message.createdTimestamp}ms.`);
+    }
+});
+
+client.login("token kalian");
